221f3f57ed38e7c16e9b8f821b14ced17e50e473
Tekena Solomon <tekena157@gmail.com>
2020-08-26 12:00:15 +0100

    added packex

---
+++ PackEx.java
@@ -0,0 +1,13 @@
+package mypackage;
+
+public class PackEx {
+	public void runEx(){
+		System.out.println("There is fire on the mountain!! ");
+	}
+	/*
+	public static void main(String[] args){
+		System.out.println("package testing!!!");
+	}
+	*/
+}
+
---
+++ RunPackEx.java
@@ -0,0 +1,8 @@
+import mypackage.PackEx;
+
+class RunPackEx{
+	public static void main(String[] args){
+		PackEx pe = new PackEx();
+		pe.runEx();
+	}
+}

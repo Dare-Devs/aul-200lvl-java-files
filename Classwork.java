37f9222fefa2d9f49f89d6df7414c22aacd17757
Tekena Solomon <tekena157@gmail.com>
2020-08-26 11:32:18 +0100

    added inheritance

---
+++ Classwork.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Classwork{
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter the radius: ");
+		double r = scan.nextFloat();
+		double circumfrence =  2*3.142*r;
+
+		System.out.println("The circumferce of the circle is: " + circumfrence);
+
+	}
+}

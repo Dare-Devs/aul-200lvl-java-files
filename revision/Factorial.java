a74b3a6a105e7ed906b26f83ee9996584e2f883e
Tekena Solomon <tekena157@gmail.com>
2020-09-30 12:57:48 +0100

    added factorial solution both recursive and non recursive

--- revision/Factorial.java
+++ revision/Factorial.java
@@ -1,14 +1,34 @@
+import java.util.Scanner;
 class Factorial {
-    int fac(int a){
-        if(a < 0){
-            System.err.println("The factorial of a negative number doesn't exist");
-        } else if (a == 1){
-            return a;
-        } else {
-            fac(a - 1);
+    // recursive method
+    static int facRec(int n){
+        if(n < 0){
+            System.err.println("Factorial of negative doesnt exist");
         }
+        if(n == 1 || n == 0){
+            return 1;
+        }
+        return n * facRec(n - 1);
     }
-    public static void main(String[] args) {
-        
+    // non recursive method
+    static int facNonRec(int n){
+        if(n < 0){
+            System.err.println("Factorial of negative doesnt exist");
+        }
+        int start = n;
+        int value = 1;
+        while(start > 1){
+            value *= start;
+            --start;
+        }
+        return value;
+    }
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num = scan.nextInt();
+
+        System.out.println(facRec(num));
+        System.out.println(facNonRec(num));
     }
 }
